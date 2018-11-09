307ae249b6afcfbeea02b57d867c41a4a221915f
mazzu <denismazzu@gmail.com>
2018-11-09 18:32:10 +0100

    add test (5!)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+import Lib
+import Semantic
+import DataStructure
+import qualified Data.Map as Map
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn . show
+  $ interpret program == Valid (Map.fromList [("x", 1), ("y", 120)])
+
+-- 5!
+program = Composition (Assignment "x" (Numeral (5)))
+  $ Composition (Assignment "y" (Numeral 1))
+  $ (While (Not (ABExp Equal (Variable "x") (Numeral 1)))
+    $ Composition (Assignment "y" (AExp Mul (Variable "y") (Variable "x")))
+    $ Assignment "x" (AExp Sub (Variable "x") (Numeral 1))
+  )
