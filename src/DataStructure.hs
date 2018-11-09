9da8c4f130b7493e63192288945ad030e9649c4b
mazzu <denismazzu@gmail.com>
2018-11-09 17:53:18 +0100

    evalBExp exec like Table 1.2

--- src/DataStructure.hs
+++ src/DataStructure.hs
@@ -64,15 +64,29 @@ data BExp = -- Table 1.2
 
 evalBExp :: BExp -> State -> Bool -- Table 1.2
 evalBExp (Boolean b) _ = b
-evalBExp (ABExp Equal a0 a1) s = (evalAExp a0 s) == (evalAExp a1 s)
-evalBExp (ABExp Greater a0 a1) s = (evalAExp a0 s) > (evalAExp a1 s)
-evalBExp (Not b) s = (evalBExp b s) == False
-evalBExp (BExp And b0 b1) s = (evalBExp b0 s) && (evalBExp b1 s)
+evalBExp (ABExp Equal a0 a1) s
+  | (evalAExp a0 s) == (evalAExp a1 s) = True
+  | (evalAExp a0 s) /= (evalAExp a1 s) = False
+evalBExp (ABExp Greater a0 a1) s
+  | (evalAExp a0 s) > (evalAExp a1 s) = True
+  | (evalAExp a0 s) <= (evalAExp a1 s) = False
+evalBExp (ABExp Smaller a0 a1) s
+  | (evalAExp a0 s) < (evalAExp a1 s) = True
+  | (evalAExp a0 s) >= (evalAExp a1 s) = False
+evalBExp (Not b) s
+  | (evalBExp b s) == False = True
+  | (evalBExp b s) == True = False
+evalBExp (BExp And b0 b1) s
+  | ((evalBExp b0 s) == True) && ((evalBExp b1 s) == True) = True
+  | ((evalBExp b0 s) == False) || ((evalBExp b1 s) == False) = False
+evalBExp (BExp Or b0 b1) s
+| ((evalBExp b0 s) == True) || ((evalBExp b1 s) == True) = True
+| ((evalBExp b0 s) == False) && ((evalBExp b1 s) == False) = False
 
 -- Operators
 
 data AOp = Sum | Sub | Mul -- Table 1.1
 
-data BOp = And -- Table 1.2
+data BOp = And | Or -- Table 1.2
 
-data ABOp = Equal | Greater -- Table 1.2
+data ABOp = Equal | Greater | Smaller -- Table 1.2
