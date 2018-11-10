51b315527356f8b9d7571a5b8a59d310c2cd02bc
mazzu <denismazzu@gmail.com>
2018-11-10 12:49:07 +0100

    add test case for while and for

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,14 +3,33 @@ import Semantic
 import DataStructure
 import qualified Data.Map as Map
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn . show
-  $ interpret program == Valid (Map.fromList [("x", 1), ("y", 120)])
+main = do
+  defaultMain (testGroup "Our Library Tests" [testWhileFact, testForLoop])
 
 -- 5!
-program = Composition (Assignment "x" (Numeral (5)))
+factParsedProgram = Composition (Assignment "x" (Numeral (5)))
   $ Composition (Assignment "y" (Numeral 1))
   $ (While (Not (ABExp Equal (Variable "x") (Numeral 1)))
     $ Composition (Assignment "y" (AExp Mul (Variable "y") (Variable "x")))
     $ Assignment "x" (AExp Sub (Variable "x") (Numeral 1))
   )
+
+-- For
+forLoopParsedProgram = Composition (Assignment "x" (Numeral (1)))
+  $ (For "i" (Numeral 0) (Numeral 5)
+    $ Assignment "x" (AExp Sum (Variable "x") (Numeral 1))
+  )
+
+testWhileFact :: TestTree
+testWhileFact = testCase "while factorial 5!"
+  (assertEqual "assert label" (Valid (Map.fromList [("x", 1), ("y", 120)])) (interpret factParsedProgram))
+
+testForLoop :: TestTree
+testForLoop = testCase "for loop" (assertEqual "label" a b)
+  where
+    a = Valid (Map.fromList [("i", 5), ("x", 6)])
+    b = interpret forLoopParsedProgram
