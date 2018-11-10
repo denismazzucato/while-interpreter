1032ec5e1d7e912b73d27baab65bee2bbbdac0a7
mazzu <denismazzu@gmail.com>
2018-11-10 12:56:44 +0100

    improvement in fixpoint

--- src/Fixpoint.hs
+++ src/Fixpoint.hs
@@ -4,7 +4,7 @@ module Fixpoint
 
 import DataStructure
 
-fix :: ((State -> State) -> (State -> State)) -> State -> State
+fix :: ((State -> State) -> State -> State) -> State -> State
 fix f = lub [ fnth f n bottom | n <- [0..] ] -- Theorem 4.37
 
 -- nth application of functional F
@@ -17,7 +17,7 @@ fnth f 0 = id
 fnth f n = f . (fnth f (n-1))
 
 lub :: [(State -> State)] -> State -> State
-lub [] s = bottom s -- Fact 4.24
+-- lub [] s = bottom s -- Fact 4.24
 lub (g:gs) s -- Lemma 4.25
-  | g s /= Undef = g s
+  | g s /= Undef = g s -- if exist g is unique
   | otherwise = lub gs s
