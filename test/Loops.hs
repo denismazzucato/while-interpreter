a8b1cf09ec6d17a5b53c1d305c234ada294b7815
mazzu <denismazzu@gmail.com>
2018-11-29 18:05:11 +0100

    fix test

--- test/Loops.hs
+++ test/Loops.hs
@@ -1,7 +1,6 @@
 module Loops (tests) where
 
 import Lib
-import IntWrapper
 import DataStructure
 
 import qualified Data.Map as Map
@@ -12,25 +11,25 @@ tests = [whileFact, repeatFact, repeatSSFact, forFact]
 
 whileFact = testCase "while factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State [("x", I 1), ("y", I 120)]
+    expected = State [("x", 1), ("y", 120)]
     result =
       interpret "x := 5; y := 1; while not x == 1 do { y := y * x; x := x - 1 }" "State []"
 
 repeatFact = testCase "repeat until factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State [("x", I 1), ("y", I 120)]
+    expected = State [("x", 1), ("y", 120)]
     result =
       interpret "x := 5; y := 1; repeat { y := y * x; x := x - 1 } until x == 1" "State []"
 
 repeatSSFact = testCase "repeat until factorial interpretation, Syntactic Sugar definition" (assertEqual "" expected result)
   where
-    expected = State [("x", I 1), ("y", I 120)]
+    expected = State [("x", 1), ("y", 120)]
     result =
       interpret "x := 5; y := 1; repeatSS { y := y * x; x := x - 1 } until x == 1" "State []"
 
 
 forFact = testCase "for factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State [("i", I 6), ("y", I 120)]
+    expected = State [("i", 6), ("y", 120)]
     result =
       interpret "y := 1; for i := 1 to 6 do y := y * i" "State []"
