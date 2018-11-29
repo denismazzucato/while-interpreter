9cc907e59c5073498b9729c24f9e3bd3b89ad5e4
mazzu <denismazzu@gmail.com>
2018-11-29 17:41:42 +0100

    fix test for new state

--- test/Loops.hs
+++ test/Loops.hs
@@ -12,25 +12,25 @@ tests = [whileFact, repeatFact, repeatSSFact, forFact]
 
 whileFact = testCase "while factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State (Map.fromList [("y", I 120), ("x", I 1)])
+    expected = State [("x", I 1), ("y", I 120)]
     result =
-      interpret "x := 5; y := 1; while not x == 1 do { y := y * x; x := x - 1 }"
+      interpret "x := 5; y := 1; while not x == 1 do { y := y * x; x := x - 1 }" "State []"
 
 repeatFact = testCase "repeat until factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State (Map.fromList [("y", I 120), ("x", I 1)])
+    expected = State [("x", I 1), ("y", I 120)]
     result =
-      interpret "x := 5; y := 1; repeat { y := y * x; x := x - 1 } until x == 1"
+      interpret "x := 5; y := 1; repeat { y := y * x; x := x - 1 } until x == 1" "State []"
 
 repeatSSFact = testCase "repeat until factorial interpretation, Syntactic Sugar definition" (assertEqual "" expected result)
   where
-    expected = State (Map.fromList [("y", I 120), ("x", I 1)])
+    expected = State [("x", I 1), ("y", I 120)]
     result =
-      interpret "x := 5; y := 1; repeatSS { y := y * x; x := x - 1 } until x == 1"
+      interpret "x := 5; y := 1; repeatSS { y := y * x; x := x - 1 } until x == 1" "State []"
 
 
 forFact = testCase "for factorial interpretation" (assertEqual "" expected result)
   where
-    expected = State (Map.fromList [("y", I 120), ("i", I 6)])
+    expected = State [("i", I 6), ("y", I 120)]
     result =
-      interpret "y := 1; for i := 1 to 6 do y := y * i"
+      interpret "y := 1; for i := 1 to 6 do y := y * i" "State []"
