e1d493e5330dd8549fbab69ddac93708e70c9164
mazzu <denismazzu@gmail.com>
2018-12-04 14:32:47 +0100

    generic cond function

--- src/UtilityFunctions.hs
+++ src/UtilityFunctions.hs
@@ -3,9 +3,9 @@ module UtilityFunctions (cond) where
 import DataStructure
 
 cond ::
-  (State -> Bool, State -> Partial State, State -> Partial State) ->
+  (State -> Bool, State -> a, State -> a) ->
   State -> -- s
-  Partial State
+  a
 cond (b, s0, s1) s
   | b s = s0 s
   | otherwise = s1 s
